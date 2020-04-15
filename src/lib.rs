c89734422980f2aa8e157ca2a1210d01d95672aa
suhanyujie <suhanyujie@qq.com>
2020-04-15 09:43:46 +0800

    模仿实现部分字符解析

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct Element {
     name: String,
@@ -6,6 +5,7 @@ struct Element {
     children: Vec<Element>,
 }
 
+// 解析出一个字母 `a`
 fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
     match input.chars().next() {
         Some('a') => Ok((&input['a'.len_utf8()..], ())),
@@ -13,13 +13,57 @@ fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
     }
 }
 
-fn match_literal(expected: &'static str) 
-    -> impl Fn(&str) -> Result<(&str, ()), &str> 
-{
+// 解析一串字符串
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
     move |input| match input.get(0..expected.len()) {
-        Some(next) if next == expected => {
-            Ok((&input[expected.len()..], ()))
-        },
+        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
         _ => Err(input),
+    }
+}
+
+// 识别标识符
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_the_letter_a() {
+        let input = "abcd";
+        let res = the_letter_a(input);
+        println!("{:?}", res);
+        assert!(res == Ok(("bcd", ())));
+    }
+
+    #[test]
+    fn test_match_literal() {
+        let parse_func = match_literal("hello world");
+        assert_eq!(Ok(("", ())), parse_func("hello world"));
+        assert_eq!(Ok((" hi ", ())), parse_func("hello world hi "));
+        assert_eq!(Err("hello Rust"), parse_func("hello Rust"));
+    }
+
+    #[test]
+    fn test_identifier() {
+        let input = "hello-world";
+        let res = identifier(input);
+        assert_eq!(Ok(("", "hello-world".to_string())), res);
     }
 }
