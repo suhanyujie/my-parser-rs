3e84bee083e603ca44d57e88701914b980aad1b1
suhanyujie <suhanyujie@qq.com>
2021-04-28 20:32:01 +0800

    try some parser

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * https://bodil.lol/parser-combinators/
 * https://www.cnblogs.com/ishenghuo/p/11180670.html
 * https://docs.rs/nom/6.0.0/nom/
+* https://github.com/fucking-translation/tutorial/tree/main/Rust/nom/nom-tutorial
--- src/nom1.rs
+++ src/nom1.rs
@@ -1,6 +1,16 @@
 extern crate nom;
 
-use nom::{IResult, bytes::complete::{is_a, is_not, tag, take_while_m_n}, character::complete::char, combinator::map_res, sequence::{delimited, terminated, tuple}};
+use nom::{
+    branch::alt,
+    bytes::complete::{is_a, is_not, tag, take_while_m_n},
+    character::complete::char,
+    combinator::{map_parser, map_res},
+    multi::{many0, many1},
+    sequence::{delimited, pair, terminated, tuple},
+    IResult,
+};
+
+use crate::whitespace_char;
 
 #[derive(Debug, PartialEq)]
 pub struct Color {
@@ -26,44 +36,84 @@ fn is_hex_digit(c: char) -> bool {
 fn hex_color(input: &str) -> IResult<&str, Color> {
     let (input, _) = tag("#")(input)?;
     let (input, (red, green, blue)) = tuple((hex_primary, hex_primary, hex_primary))(input)?;
-    Ok((input, Color {red, green, blue}))
+    Ok((input, Color { red, green, blue }))
 }
 
-fn md_title_level(input: &str) -> IResult<&str, (&str, &str, &str, &str)> {
-    tuple((is_a("#"), is_a(" "), is_not("\n"), is_a("\r\n")))(input)
+fn md_title_level(input: &str) -> IResult<&str, (&str, &str)> {
+    tuple((is_a("#"), is_a(" ")))(input)
 }
 
 // fn md_title_content(input: &str) -> IResult<&str, &str> {
 //     terminated(is_not("\r\n"), "\n")(input)
 // }
 
-/// md doc title: 
+fn md_title_content_part(input: &str) -> IResult<&str, &str> {
+    is_not("\n")(input)
+}
+
+fn md_title_code_block(input: &str) -> IResult<&str, &str> {
+    delimited(char('`'), is_not("`"), char('`'))(input)
+}
+
+fn md_title_content(input: &str) -> IResult<&str, Vec<&str>> {
+    // map_parser(md_title_content_part, md_title_code_block)
+    many1(alt((md_title_content_part, md_title_code_block)))(input)
+}
+
+// fn many_part_content<'a, 'b>(input: &'a str) -> IResult<&'a str, &'b str>
+// where
+//     'b: 'a,
+// {
+//     let parser = many1(alt((md_title_content_part, md_title_code_block)));
+//     let mut result = String::new();
+//     if let Ok((remain, res_list)) = parser(input) {
+//         result = res_list.join("");
+//     }
+//     let res_ref: &'b str = &*result;
+//     return Ok(("", res_ref));
+// }
+
+// todo
+fn md_title_content_dealed(input: &str) -> IResult<&str, Vec<&str>> {
+    // map_parser(md_title_content_part, md_title_code_block)
+    many1(alt((md_title_content_part, md_title_code_block)))(input)
+}
+
+/// md doc title:
 /// * # title one
 /// * ## title two
-fn md_title(input: &str) -> IResult<&str, &str> {
-   delimited(tag("#"), is_not("\n"), char('\n'))(input)
-}
+// fn md_title(input: &str) -> IResult<&str, &str> {
+//     delimited(tag("#"), many1(" "), char('\n'))(input)
+// }
 
 mod tests {
     use super::*;
 
     #[test]
-    fn test_title_level() {
-        assert_eq!(
-            md_title_level("# MySQL 技术内幕\n* 第一个"),
-            Ok(("* 第一个", ("#", " ", "MySQL 技术内幕", "\n")))
-        )
+    fn test_md_title_content() {
+        let text = "## `MySQL` 技术内幕 `internal`\n* 第一个";
+        let mut result = String::new();
+        if let Ok(content_info) = md_title_content(text) {
+            result = content_info.1.join("");
+        }
+        println!("the title content is: {}", result);
     }
 
     #[test]
-    fn test_md_title() {
-        assert_eq!(md_title("# MySQL 技术内幕\n* 第一个\n* 第二个"),
-            Ok((
-                "",
-                "# MySQL 技术内幕"
-            ))
+    fn test_title_level() {
+        assert_eq!(
+            md_title_level("## MySQL 技术内幕\n* 第一个"),
+            Ok(("* 第一个", ("#", " ")))
         );
     }
+
+    // #[test]
+    // fn test_md_title() {
+    //     assert_eq!(
+    //         md_title("# MySQL 技术内幕\n* 第一个\n* 第二个"),
+    //         Ok(("", "# MySQL 技术内幕"))
+    //     );
+    // }
 
     #[test]
     fn test_from_hex() {
