39e520768fd3dccbdca463f8629d7f76ae107773
suhanyujie <suhanyujie@qq.com>
2021-09-05 23:56:17 +0800

    complete the post for parser1

--- src/parse_num1.rs
+++ src/parse_num1.rs
@@ -1,5 +1,5 @@
 /*
->* 文章名称：从零编写一个解析器
+>* 文章名称：从零编写一个解析器（1）
 >* 参考地址：https://github.com/Geal/nom/blob/master/doc/making_a_new_parser_from_scratch.md
 >* 文章来自：https://github.com/suhanyujie/my-parser-rs
 >* 标签：Rust，parser
@@ -17,10 +17,10 @@
 根据 [文档](https://github.com/Geal/nom/blob/master/doc/making_a_new_parser_from_scratch.md) 中的介绍，我们先从解析一个括号中的数字 —— `(12345)` 开始。先定义一个函数签名，它用于把字符串 `(12345)` 解析成数字：
 
 ```rust
-fn parse_u32(input: &[u8]) -> nom::IResult<&[u8], u32>
+fn parse_u32(input: &[u8]) -> IResult<&[u8], u32>
 ```
 
-parse_u32 是函数名称，它接收一个 input  参数，nom::IResult 是 nom 中常用的结果返回类型。可以通过[文档](https://docs.rs/nom/7.0.0/nom/type.IResult.html)查看其声明和注释：
+parse_u32 是函数名称，它接收一个 input  参数，`IResult` 即 `nom::IResult`，是 nom 中常用的结果返回类型。可以通过[文档](https://docs.rs/nom/7.0.0/nom/type.IResult.html)查看其声明和注释：
 
 ```
 /// Holds the result of parsing functions
@@ -40,10 +40,11 @@ pub type IResult<I, O, E = error::Error<I>> = Result<(I, O), Err<E>>;
 
 基于 nom 的解析器时，大都是自下而上构建的，先编写最小的解析单元，然后使用组合子将它们组合到更复杂的解析器中。
 nom 中已经提供了很多的基础的[解析单元](https://docs.rs/nom/7.0.0/nom/character/complete/index.html)。利用这些解析单元，我们可以做两种选择：
+
 * 1.解析特定的内容
 * 2.组合更上层的解析器
 
-围绕这两点，我们可以开始尝试 —— 解析 `(12345)`
+围绕这两点，我们可以先开始尝试 —— 解析 `(12345)`
 
 很明显，我们无法直接用基础的解析器直接解析出 `(12345)` 中的数字部分，因为**基础解析器**解析的内容是比较单调的，比如可以用来解析 `aaa`，`97900` 等这类比较由规律的单元。
 
@@ -94,21 +95,98 @@ fn test_tag1() {
 
 好了，虽然有点初级，但至少我们起步了！
 
+## 加速
+虽然我们可以通过简单的解析器解析出需要的字符串，但我们的目标可不是单纯地解析出 `(` 或者 `##` 之类地单调字符，我们的首要目标是解析出字符串（`(12345)`）中括号中的数字！
 
+此时，我们就需要用到组合子。通过组合子对不同基础解析器的组合，可以组合出更复杂的解析器。
 
+nom 仓库中提供了一个分类组合子的[文档](https://github.com/Geal/nom/blob/master/doc/choosing_a_combinator.md)。
 
+从其中，我们可以找到一个适用于我们场景的组合子，例如：[delimited](https://docs.rs/nom/7.0.0/nom/sequence/fn.delimited.html)
+文档中对该解析器的描述是：用第一个解析器匹配一个对象，然后丢弃它；然后用第二个解析器匹配特定的内容，并获取它；最后用第三个解析器匹配对象，并将对象丢弃。
+刚好适用于我们的 `(12345)`
+
+* 1.用第一个解析器解析出 `(`，并丢弃；
+* 2.然后用第二个解析器匹配出 `12345`；
+* 3.最后用第三个解析器解析出 `)` 并丢弃。
+
+用代码实现如下：
+
+```rust
+fn parse_u32(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    delimited(tag("("), digit0, tag(")"))(input)
+}
+```
+
+是的，只有一行代码，就能实现解析 `(12345)`。这个函数中，我们可能对 `digit0` 比较陌生，它是 delimited 函数中的第二个解析器，nom 中[封装好的](https://docs.rs/nom/7.0.0/nom/character/complete/fn.digit0.html)。
+它位于 `nom::character::complete::digit0`。
+
+## 起飞
+细心的朋友可能注意到，`parse_u32` 函数返回值是 `IResult<&[u8], &[u8]>`，也就是说，在成功时，它返回的输入的剩余数据的类型是 `&[u8]`；解析的结果的类型也是 `&[u8]`，
+这是否和我们所说的拿到**数值**不太一致？
+
+是的，文章开头，我们的函数签名是 `fn parse_u32(input: &[u8]) -> IResult<&[u8], u32>`。
+我们期望剩余的输入是 `&[u8]`，解析的结果是 `u32` 类型。因此，我们需要在基于 parse_u32 的基础上，将 `&[u8]` 类型的解析结果转换成 String，再将字符串转换成 u32 类型：
+
+```
+fn parse_u32_ver1(input: &[u8]) -> IResult<&[u8], u32> {
+    let mut my_parser = delimited(tag("("), digit0, tag(")"));
+    let res = my_parser(input);
+    match res {
+        Ok((remain, raw)) => {
+            let s1 = String::from_utf8_lossy(raw);
+            let num: u32 = s1.parse().unwrap();
+            Ok((remain, num))
+        }
+        Err(err) => Err(err),
+    }
+}
+```
+
+我们通过 `String::from_utf8_lossy(raw);` 将解析结果编码成字符串，通过 `let num: u32 = s1.parse().unwrap();` 将字符串转换成 u32 类型。
+
+写个单测验证一下：
+
+```rust
+fn test_parse_u32_ver1() {
+    assert_eq!(
+        parse_u32_ver1("(12345)".as_bytes()),
+        Ok(("".as_bytes(), 12345))
+    );
+
+    assert_eq!(parse_u32_ver1("(0)".as_bytes()), Ok(("".as_bytes(), 0)));
+}
+```
+
+太棒了，验证通过！
+
+至此，总算是完成了一个简单的解析器，但距离解析 sql、json 还很远，我们下一章来试试如何解析 sql；
 
 ## 参考
 * https://github.com/Geal/nom/blob/master/doc/making_a_new_parser_from_scratch.md
 
 */
 
-use nom::character::complete::{alphanumeric0, char, digit0};
+use nom::bytes::complete::tag;
+use nom::character::complete::digit0;
 use nom::sequence::delimited;
-use nom::{bytes::complete::take, character::is_digit, number::complete::be_u32, IResult};
+use nom::IResult;
 
 fn parse_u32(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    delimited(char('('), digit0, char(')'))(input)
+    delimited(tag("("), digit0, tag(")"))(input)
+}
+
+fn parse_u32_ver1(input: &[u8]) -> nom::IResult<&[u8], u32> {
+    let mut my_parser = delimited(tag("("), digit0, tag(")"));
+    let res = my_parser(input);
+    match res {
+        Ok((remain, raw)) => {
+            let s1 = String::from_utf8_lossy(raw);
+            let num: u32 = s1.parse().unwrap();
+            Ok((remain, num))
+        }
+        Err(err) => Err(err),
+    }
 }
 
 #[cfg(test)]
@@ -116,6 +194,16 @@ mod tests {
     use nom::bytes::complete::tag;
 
     use super::*;
+
+    #[test]
+    fn test_parse_u32_ver1() {
+        assert_eq!(
+            parse_u32_ver1("(12345)".as_bytes()),
+            Ok(("".as_bytes(), 12345))
+        );
+
+        assert_eq!(parse_u32_ver1("(0)".as_bytes()), Ok(("".as_bytes(), 0)));
+    }
 
     #[test]
     fn test_tag1() {
