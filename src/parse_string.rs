955de4b79a3220408204be8a0639ea405bee0718
suhanyujie <suhanyujie@qq.com>
2021-09-07 23:56:14 +0800

    update post for parse string

--- src/parse_string.rs
+++ src/parse_string.rs
@@ -1,6 +1,8 @@
-/*!
-代码主题：字符串解析
-参考地址：https://github.com/Geal/nom/blob/master/examples/string.rs
+/*
+>* 文章名称：从零编写一个解析器（2）—— 字符串解析
+>* 参考地址：https://github.com/Geal/nom/blob/master/examples/string.rs
+>* 文章来自：https://github.com/suhanyujie/my-parser-rs
+>* 标签：Rust，parser
 
 在代码中，我们经常会声明变量、声明字符串，然后编写业务逻辑，然后你是否有想过，编译器是如何读懂你的变量声明，你的代码逻辑。
 在这个文章中，我们先从字符串开始，了解如何通过代码来识别代码，解析你所编写的内容是什么。
@@ -12,9 +14,136 @@
 Rust 的字符串是 utf-8 编码，长度可动态增长的类型，它在底层通常是由一些列的字节序列构成，经过一些特定的编码后，就能得到你想要的字符串了。
 
 此外，在 Rust 字符串中，还支持携带转义的 Unicode 字符，如：`String::from("\u{1231}")`
+因此，我们先确定好支持的常见情况，以及一些特殊的情况。
 
+### 常见情况
+所谓通用的情况，就是最常见的字符串字面量，如：`"hello world"`。通过前面的[实践](https://github.com/suhanyujie/my-parser-rs/blob/master/src/parse_num1.rs)，我们可以很快地写出解析该字符串的解析器：
+
+```rust
+fn parse_normal_str1<'a>(input: &'a str) -> IResult<&'a str, String> {
+    let mut parser = delimited(tag("\""), is_not("\n\""), tag("\""));
+    let res = parser(input);
+    match res {
+        Ok((remain, result)) => {
+            return Ok((remain, result.to_string()));
+        }
+        Err(err) => {
+            return Err(err);
+        }
+    }
+}
+```
+
+通过解析器 `parse_normal_str1`，将源字符串解析，返回剩余部分，以及解析出的字符串结果。我们视所有的字符串都是以 `"` 开始，以 `"` 结束。
+只需通过 [delimited](https://docs.rs/nom/7.0.0/nom/sequence/fn.delimited.html) 我们可以很容易地将字符串从 `""` 中分离出来。运行一个测试用例：
+
+```
+ fn test1() {
+    let s = r##""hello world""##;
+    assert_eq!(parse_normal_str1(s), Ok(("", "hello world".to_string())));
+}
+```
+
+一如既往地棒，测试通过：
+
+```
+running 1 test
+test parse_string::tests::test1 ... ok
+```
+
+可是，你是否考虑过类似于 `"hello \"Nico\""` 这种中间带有转义符的字符串？先把它放到测试用例中跑一跑：
+
+```
+fn test2() {
+    assert_eq!(
+        parse_normal_str1(r##""hello \"Nico\"""##),
+        Ok(("", "hello \"Nico\"".to_string()))
+    );
+}
+```
+
+做好准备，编译器对我们发起攻击了，给出提示信息：
+
+```
+thread 'parse_string::tests::test2' panicked at 'assertion failed: `(left == right)`
+  left: `Ok(("Nico\\\"\"", "hello \\"))`,
+  right: `Ok(("", "hello \"Nico\""))`', src/parse_string.rs:107:9
+```
+
+可以看到，解析器只解析到了转义符，遇到转义符后的 `"` 就停止了，把 `Nico\\\"\"` 当作解析后的剩余部分返回，得到解析结果是 `hello \\`，完全不符合预期。
+
+怎么办？
+
+要想支持解析带有转义字符的字符串，我们先看看如何解析转义字符。
+
+### 转义字符
+
+
+
+
+### 特殊情况
+
+## 参考
+* https://github.com/Geal/nom/blob/master/examples/string.rs
 
 */
+
+use nom::character::complete::char as nom_char;
+use nom::combinator::value;
+use nom::error::{FromExternalError, ParseError};
+use nom::{
+    branch::alt,
+    bytes::complete::{is_not, tag},
+    sequence::{delimited, preceded},
+    IResult,
+};
+
+use crate::sql::Literal;
+
+fn parse_normal_str1<'a>(input: &'a str) -> IResult<&'a str, String> {
+    let mut parser = delimited(tag("\""), is_not("\n\""), tag("\""));
+    let res = parser(input);
+    match res {
+        Ok((remain, result)) => {
+            return Ok((remain, result.to_string()));
+        }
+        Err(err) => {
+            return Err(err);
+        }
+    }
+}
+
+fn parse_escaped_str1<'a>(input: &'a str) -> IResult<&'a str, char> {
+    let mut parser = preceded(
+        nom_char('\\'),
+        alt((value('\"', nom_char('"')), value('\n', nom_char('n')))),
+    );
+    match parser(input) {
+        Ok((remain, res_char)) => {
+            return Ok((remain, res_char));
+        }
+        Err(err) => {
+            return Err(err);
+        }
+    }
+}
+
+fn parse_str_with_escaped<'a>(input: &'a str) -> IResult<&'a str, String> {
+    let mut parser = delimited(tag("\""), is_not("\n\""), tag("\""));
+    let res = parser(input);
+    match res {
+        Ok((remain, result)) => {
+            return Ok((remain, result.to_string()));
+        }
+        Err(err) => {
+            return Err(err);
+        }
+    }
+}
+
+enum Fr<'a> {
+    Literal(&'a str),
+}
 
 fn demo1() {
     println!("{}", String::from("\u{1231}")); // ሱ
@@ -23,10 +152,36 @@ fn demo1() {
 
 #[cfg(test)]
 mod tests {
+    use nom::character::is_alphabetic;
+
+    use crate::map;
+
     use super::*;
 
     #[test]
     fn test1() {
-        demo1();
+        let s = r##""hello world""##;
+        assert_eq!(parse_normal_str1(s), Ok(("", "hello world".to_string())));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            parse_normal_str1(r##""hello \"Nico\"""##),
+            Ok(("", "hello \"Nico\"".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_escaped_str() {
+        assert_eq!(parse_escaped_str1(r##"\""##), Ok(("", '"')));
+    }
+
+    #[test]
+    fn test_enum1() {
+        // let s1 = "hello";
+        // let mut p: dyn FnMut(&str) -> IResult<&str, Fr> =
+        //     nom::combinator::map(tag("hello"), Fr::Literal);
+        // let res = p(s1);
     }
 }
--- src/sql.rs
+++ src/sql.rs
@@ -1,1 +1,173 @@
+/*
+>* 文章名称：从零编写一个解析器（2）—— 解析 MySQL 建表语句
+>* 参考地址：https://github.com/Geal/nom/blob/master/doc/making_a_new_parser_from_scratch.md
+>* 文章来自：https://github.com/suhanyujie/my-parser-rs
+>* 标签：Rust，parser
 
+根据 MySQL [官方文档](https://dev.mysql.com/doc/refman/8.0/en/create-table.html)，我们可以了解一下 MySQL 创建表的语法。
+由于我们的需求比较简单，只针对特定的建表语句，解析出字段结构，并转成 go struct 即可，因此我们不需要实现所有的建表语法。只需实现解析最常规的 sql 语句。
+
+```
+CREATE [TEMPORARY] TABLE [IF NOT EXISTS] tbl_name
+    (create_definition,...)
+    [table_options]
+    [partition_options]
+```
+
+我们甚至可以简化成：
+
+```
+CREATE TABLE tbl_name
+    (create_definition,...)
+    [table_options]
+    [partition_options]
+```
+
+也许你对 MySQL 建表语句很熟悉，但可能你对解析 sql 语句不会很熟练，不过没关系，就像你编写大型程序一样，对其进行模块拆分，然后各个击破。
+
+上面的建表语句规则中，我们先从 `create_definition` 开始，它的定义如下：
+
+```
+create_definition: {
+    col_name column_definition
+  | {INDEX | KEY} [index_name] [index_type] (key_part,...)
+      [index_option] ...
+  | {FULLTEXT | SPATIAL} [INDEX | KEY] [index_name] (key_part,...)
+      [index_option] ...
+  | [CONSTRAINT [symbol]] PRIMARY KEY
+      [index_type] (key_part,...)
+      [index_option] ...
+  | [CONSTRAINT [symbol]] UNIQUE [INDEX | KEY]
+      [index_name] [index_type] (key_part,...)
+      [index_option] ...
+  | [CONSTRAINT [symbol]] FOREIGN KEY
+      [index_name] (col_name,...)
+      reference_definition
+  | check_constraint_definition
+}
+
+column_definition: {
+    data_type [NOT NULL | NULL] [DEFAULT {literal | (expr)} ]
+      [VISIBLE | INVISIBLE]
+      [AUTO_INCREMENT] [UNIQUE [KEY]] [[PRIMARY] KEY]
+      [COMMENT 'string']
+      [COLLATE collation_name]
+      [COLUMN_FORMAT {FIXED | DYNAMIC | DEFAULT}]
+      [ENGINE_ATTRIBUTE [=] 'string']
+      [SECONDARY_ENGINE_ATTRIBUTE [=] 'string']
+      [STORAGE {DISK | MEMORY}]
+      [reference_definition]
+      [check_constraint_definition]
+  | data_type
+      [COLLATE collation_name]
+      [GENERATED ALWAYS] AS (expr)
+      [VIRTUAL | STORED] [NOT NULL | NULL]
+      [VISIBLE | INVISIBLE]
+      [UNIQUE [KEY]] [[PRIMARY] KEY]
+      [COMMENT 'string']
+      [reference_definition]
+      [check_constraint_definition]
+}
+```
+
+## 参考
+* https://dev.mysql.com/doc/refman/8.0/en/create-table.html
+
+*/
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum SqlType {
+    Bool,
+    Char(u16),
+    Varchar(u16),
+    Int(u16),
+    UnsignedInt(u16),
+    Bigint(u16),
+    UnsignedBigint(u16),
+    Tinyint(u16),
+    UnsignedTinyint(u16),
+    Blob,
+    Longblob,
+    Mediumblob,
+    Tinyblob,
+    Double,
+    Float,
+    Real,
+    Tinytext,
+    Mediumtext,
+    Longtext,
+    Text,
+    Date,
+    DateTime(u16),
+    Timestamp,
+    Binary(u16),
+    Varbinary(u16),
+    Enum(Vec<Literal>),
+    Decimal(u8, u8),
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Literal {
+    Null,
+    Integer(i64),
+    UnsignedInteger(u64),
+    FixedPoint(Real),
+    String(String),
+    Blob(Vec<u8>),
+    CurrentTime,
+    CurrentDate,
+    CurrentTimestamp,
+    Placeholder(ItemPlaceholder),
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum ItemPlaceholder {
+    QuestionMark,
+    DollarNumber(i32),
+    ColonNumber(i32),
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Real {
+    pub integral: i32,
+    pub fractional: i32,
+}
+
+fn parse_sql1() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// 字符串字面量的声明可以参考 https://doc.rust-lang.org/reference/tokens.html#raw-string-literals
+    #[test]
+    fn test_parse_sql1() {
+        let sql_str1 = r#"CREATE TABLE parse_t(
+            id INT NOT NULL,
+            cv1 VARCHAR(20) DEFAULT "",
+            tt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            dayweek SMALLINT,
+            cv2 INT,
+            PRIMARY KEY(id)
+        ) ENGINE=innodb DEFAULT CHARSET=utf8mb4;"#;
+        let lines: Vec<String> = sql_str1
+            .lines()
+            .filter(|l| {
+                !l.is_empty()
+                    && !l.starts_with("#")
+                    && !l.starts_with("--")
+                    && !(l.starts_with("/*") && l.ends_with("*/;"))
+            })
+            .map(|l| {
+                if !(l.ends_with("\n") || l.ends_with(";")) {
+                    String::from(l) + "\n"
+                } else {
+                    String::from(l)
+                }
+            })
+            .collect();
+        //let res = parse_queryset(&lines);
+
+        println!("lines: {:?}", &lines);
+    }
+}
