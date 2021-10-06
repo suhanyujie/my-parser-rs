abd7879691b034bd2f3090be2177d61fb85117c4
suhanyujie <suhanyujie@qq.com>
2021-10-06 21:12:09 +0800

    将渲染过程进行封装

--- src/sql2_render.rs
+++ src/sql2_render.rs
@@ -3,7 +3,9 @@
 >* 文章来自：https://github.com/suhanyujie/my-parser-rs
 >* 标签：Rust，parser
 
-紧接着上一篇的 sql 解析，当我们把建表 SQL 语句中重要信息解析出来以后，我们就能将其渲染为实际需要的结构，比如：json、go struct 等。
+紧接着上一篇的 sql 解析，当我们把建表 SQL 语句中重要信息解析出来以后，我们就能将其渲染为实际需要的结构，比如：json、go struct 等。在这个文章中，我们将尝试将其渲染成 go struct 的结构。
+
+
 
 
 */
@@ -13,6 +15,7 @@ extern crate tera;
 use std::collections::HashMap;
 
 use crate::sql1::{DataTypeEnum, OneColumn};
+use serde::Serialize;
 use serde_json::from_value;
 use serde_json::to_string;
 use serde_json::Error;
@@ -128,22 +131,89 @@ pub fn transfer_type_helper(args: &HashMap<String, Value>) -> tera::Result<Value
     Ok(serde_json::json!(typ))
 }
 
-// 定义一个渲染器
+/// 定义一个渲染器，通过“渲染器”可以将解析好的 create sql 转换成需要的结构和数据。
 #[derive(Debug)]
-struct TypeRender<'a> {
-    tera: &'a Tera,
+pub struct TypeRender {
+    tera: Tera,
+    tera_ctx: tera::Context,
+    raw_tpl: Option<String>,
     setting: RenderSetting,
+}
+
+impl TypeRender {
+    pub fn new() -> Self {
+        let mut tera = match Tera::new("./data/*.tpl") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.register_function("transfer_type_helper", transfer_type_helper);
+        tera.register_function("to_big_case_camel_helper", to_big_case_camel_helper);
+        tera.register_function("to_small_case_camel_helper", to_small_case_camel_helper);
+
+        let setting = RenderSetting {
+            field_name_style: FieldNameStyleEnum::SmallCaseCamel,
+            need_json_tag: true,
+            need_form_tag: false,
+            need_gorm_tag: false,
+        };
+
+        let mut ctx = Context::new();
+        TypeRender {
+            tera,
+            setting,
+            tera_ctx: ctx,
+            raw_tpl: None,
+        }
+    }
+
+    /// 设置字符串模板
+    pub fn set_raw_tpl(&mut self, raw_tpl: String) -> &mut Self {
+        self.raw_tpl = Some(raw_tpl);
+        self
+    }
+
+    /// 设置变量值
+    pub fn set_var<V: Serialize + ?Sized, K: Into<String>>(
+        &mut self,
+        key: K,
+        val: &V,
+    ) -> &mut Self {
+        self.tera_ctx.insert(key, val);
+        self
+    }
+
+    /// 渲染
+    pub fn render(&mut self) -> tera::Result<String> {
+        let tpl = self.raw_tpl.as_deref().unwrap_or("");
+        return self.tera.render_str(tpl, &self.tera_ctx);
+    }
+}
+
+impl Default for TypeRender {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 /// 渲染设置
 #[derive(Debug)]
-struct RenderSetting {
-    field_name_style: FieldNameStyleEnum,
+pub struct RenderSetting {
+    /// 字段名风格。默认小驼峰 —— SmallCaseCamel
+    pub field_name_style: FieldNameStyleEnum,
+    /// 是否需要 json tag
+    pub need_json_tag: bool,
+    /// 是否需要 form tag
+    pub need_form_tag: bool,
+    /// 是否需要 gorm tag
+    pub need_gorm_tag: bool,
 }
 
 // field name 风格
 #[derive(Debug)]
-enum FieldNameStyleEnum {
+pub enum FieldNameStyleEnum {
     // 小驼峰
     SmallCaseCamel,
     // 大驼峰
@@ -183,13 +253,7 @@ mod tests {
         tera.register_function("to_big_case_camel_helper", to_big_case_camel_helper);
         tera.register_function("to_small_case_camel_helper", to_small_case_camel_helper);
         let mut context = Context::new();
-        let f1 = OneColumn {
-            name: "id".to_string(),
-            typ: DataTypeEnum::Bigint,
-            comment: "主键".to_string(),
-        };
-        let field_arr = vec![f1];
-
+        let field_arr = get_test_field_arr();
         context.insert("field_arr", &field_arr);
         let struct_str = r###"type PpmOrgCustomerTrace struct {
 {{field_str}}
@@ -208,5 +272,34 @@ mod tests {
             println!("{}", &res_str);
         }
         assert!(&struct_result.is_ok());
+    }
+
+    #[test]
+    fn test_type_render() {
+        let mut tr = TypeRender::new();
+
+        let type_tpl = r###"{% for field in field_arr %}
+    {{to_big_case_camel_helper(word=field.name)}} {{transfer_type_helper(typ=field.typ)}} `json:"{{to_small_case_camel_helper(word=field.name)}}"`
+{% endfor %}
+"###;
+        let field_arr = get_test_field_arr();
+        let rendered_res = tr
+            .set_raw_tpl(type_tpl.to_string())
+            .set_var("field_arr", &field_arr)
+            .render();
+        if let Ok(res_str) = &rendered_res {
+            println!("{}", &res_str);
+        }
+        assert!(&rendered_res.is_ok());
+    }
+
+    fn get_test_field_arr() -> Vec<OneColumn> {
+        let f1 = OneColumn {
+            name: "id".to_string(),
+            typ: DataTypeEnum::Bigint,
+            comment: "主键".to_string(),
+        };
+        let field_arr = vec![f1];
+        return field_arr;
     }
 }
